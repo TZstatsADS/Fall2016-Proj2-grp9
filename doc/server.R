559b4bc8e98d90efc0ee58f5adaddfbac55b80b3
Yu Qin <qinyu1119@yahoo.com>
2016-10-11 22:04:03 -0400

    server updated
    Yu Qin

--- doc/server.R
+++ doc/server.R
@@ -1,5 +1,16 @@
-#Server#
-shinyServer(function(input, output) {
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(dplyr)
+library(ggplot2)
+
+shinyServer(function(input, output, session) {
+  
+  ## Interactive Map ###########################################
+  
+  # Create the map
+  
+  
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles(
@@ -9,110 +20,245 @@ shinyServer(function(input, output) {
       setView(lng = -73.97, lat = 40.75, zoom = 13)
   })
   
-#Stations status (TimeRange)#
-  Sstatus<-reactive({
-   status<-data_Time[[input$TimeRange]]
-   nuba<-status$Num_bikes_available
-   nuda<-status$Num_dock_available
-   slat_a<-slat[nuba!=0&nuda!=0]
-   slon_a<-slon[nuba!=0&nuda!=0]
-   sname_a<-sname[nuba!=0&nuda!=0]
-   nuba_a<-nuba[nuba!=0&nuda!=0]
-   nuda_a<-nuda[nuba!=0&nuda!=0]
-   slat_u<-slat[nuba==0|nuda==0]
-   slon_u<-slon[nuba==0|nuda==0]
-   sname_u<-sname[nuba==0|nuda==0]
-   nuba_u<-nuba[nuba==0|nuda==0]
-   nuda_u<-nuda[nuba==0|nuda==0]
-   S_a<-data.frame(slat_a,slon_a,sname_a,nuba_a,nuda_a)
-   S_u<-data.frame(slat_u,slon_u,sname_u,nuda_u,nuda_u)
-   return(list(S_a=S_a,S_u=S_u))})
-
-
-#Initial plots#
-observe({
-  leafletProxy("map") %>%
-  clearMarkers() %>%
-  addMarkers(data=Sstatus()[["S_a"]],~Sstatus()[["S_a"]]$slon_a,~Sstatus()[["S_a"]]$slat_a,icon=Bikeicons1,popup=~paste("Name: ",Sstatus()[["S_a"]]$sname_a,"<br>",as.character(Sstatus()[["S_a"]]$nuba_a)," bikes available, ",as.character(Sstatus()[["S_a"]]$nuda_a),"docks available"))%>%
-  addMarkers(data=Sstatus()[["S_u"]],~Sstatus()[["S_u"]]$slon_u,~Sstatus()[["S_u"]]$slat_u,icon=Bikeicons2,popup=~paste("Name: ",Sstatus()[["S_u"]]$sname_u,"<br>",as.character(Sstatus()[["S_u"]]$nuba_u)," bikes available, ",as.character(Sstatus()[["S_u"]]$nuda_u),"docks available"))
-})
-#2 addresses part#
-observe(
-{
-  if(input$submit[1]>0)
-  {
-  add_origin<-geocode(input$address1)
-  add_destination<-geocode(input$address2)
-  rlat<-add_origin[2]
-  rlon<-add_origin[1]
-  glat<-add_destination[2]
-  glon<-add_destination[1]
-  r1<-Nearest3(rlat,rlon,slat,slon,Sstatus()[["S_a"]]$nuba,Sstatus()[["S_a"]]$nuda,sname)
-  g1<-Nearest3(glat,glon,slat,slon,Sstatus()[["S_a"]]$nuba,Sstatus()[["S_a"]]$nuda,sname)
-  slat1<-c(r1$p1[1],r1$p2[1],r1$p3[1],g1$p1[1],g1$p2[1],g1$p3[1])
-  slon1<-c(r1$p1[2],r1$p2[2],r1$p3[2],g1$p1[2],g1$p2[2],g1$p3[2])
-  nuba1<-c(r1$p1[3],r1$p2[3],r1$p3[3],g1$p1[3],g1$p2[3],g1$p3[3])
-  nuda1<-c(r1$p1[4],r1$p2[4],r1$p3[4],g1$p1[4],g1$p2[4],g1$p3[4])
-  sname1<-c(r1$p1[5],r1$p2[5],r1$p3[5],g1$p1[5],g1$p2[5],g1$p3[5])
-position1<-data.frame(slon1,slat1)
-leafletProxy("map")%>%
-  clearMarkers() %>%
-  addMarkers(rlon,rlat,popup="Start")%>%addMarkers(glon,glat,popup="End")%>%
-  addMarkers(data=position1,~slon1,~slat1,icon=Bikeicons1,popup=~paste("Name: ",sname1,"<br>",as.character(nuba1)," bikes available, ",as.character(nuda1),"docks available"))
-}})
-#Click event#
-click<-reactive({
-event1<-input$map_marker_click
-if (is.null(event1))
-{return()}
-event2<-input$map_marker_click
-if (is.null(event1))
-{return()}
-ulat <- event1$lat
-ulon <- event1$lng
-vlat<-event2$lat
-vlon<-event2$lng
-u<-data.frame(ulat,ulon)
-v<-data.frame(vlat,vlon)
-return(list(u=u,v=v))
-})
-
-#One Stop or not#
-observe({
-if(input$OneStop=="FALSE")
-{ 
-#Google Api#
-  wed_add<-sprintf("https://maps.googleapis.com/maps/api/distancematrix/json?origins=%f,%f&destinations=%f,%f&mode=bicycling&key=%s",click()[["u"]]$ulat,click()[["u"]]$ulon,click()[["v"]]$vlat,click()[["v"]]$vlon,Api_key)
-  Route<-fromJSON(wed_add,simplify = FALSE)
-  Distances<-Route$rows[[1]]$elements[[1]]$distance$text
-  Times<-Route$rows[[1]]$elements[[1]]$duration$text 
-  poly <- geoRoute(click()[["v"]]$vlat,click()[["v"]]$vlon,click()[["u"]]$ulat,click()[["u"]]$ulon)
-  llatlon <- decodeLine(poly)
-  leafletProxy("map") %>%
-    clearShapes() %>%
-    addPolylines(lng=llatlon$lon,lat=llatlon$lat,color="blue",popup=sprintf("Time=%s, Distance=%s",Times,Distances))
-}
-if(input$OneStop=="TRUE")
-{ 
-  mid_lat<-(click()[["u"]]$ulat+click()[["v"]]$vlat)/2
-  mid_lon<-(click()[["u"]]$ulon+click()[["v"]]$vlon)/2
-  mid1<-Nearest3(mid_lat,mid_lon,slat,slon,Sstatus[["S_a"]]$nuba,Sstatus[["S_a"]]$nuda,sname)
-  poly1<- geoRoute(click()[["u"]]$ulat,click()[["u"]]$ulon,mid1$p1[1],mid1$p1[2])
-  poly2 <- geoRoute(mid1$p1[1],mid1$p1[2],click()[["v"]]$vlat,click()[["v"]]$vlon)
-  llatlon1 <- decodeLine(poly1)
-  llatlon2 <- decodeLine(poly2)
- #Google Api#
-  wed_add1<-sprintf("https://maps.googleapis.com/maps/api/distancematrix/json?origins=%f,%f&destinations=%f,%f&mode=bicycling&key=%s",click()[["u"]]$ulat,click()[["u"]]$ulon,mid1$p1[1],mid1$p1[2],Api_key)
-  Route1<-fromJSON(wed_add1,simplify = FALSE)
-  Distances1<-Route1$rows[[1]]$elements[[1]]$distance$text
-  Times1<-Route1$rows[[1]]$elements[[1]]$duration$text
-  wed_add2<-sprintf("https://maps.googleapis.com/maps/api/distancematrix/json?origins=%f,%f&destinations=%f,%f&mode=bicycling&key=%s",mid1$p1[1],mid1$p1[2],click()[["v"]]$vlat,click()[["v"]]$vlon,Api_key)
-  Route2<-fromJSON(wed_add2,simplify = FALSE)
-  Distances2<-Route2$rows[[1]]$elements[[1]]$distance$text
-  Times2<-Route2$rows[[1]]$elements[[1]]$duration$text
-  leafletProxy("map") %>% clearShapes() %>%
-  addMarkers(mid1$p1[2],mid1$p1[1],popup=paste("Mid, ","<br>","Name: ",mid1$p1[5],"<br>",as.character(mid1$p1[3])," bikes available, ",as.character(mid1$p1[4]),"docks available"))%>%
-  addPolylines(lng=llatlon1$lon,lat=llatlon1$lat,color="blue",popup=sprintf("Time=%s, Distance=%s",Times1,Distances1))%>%
-  addPolylines(lng=llatlon2$lon,lat=llatlon2$lat,color="red",popup=sprintf("Time=%s, Distance=%s",Times2,Distances2))
-}})
+  
+  # Select toilet type, multiple selections are allowed
+  ttype <- reactive({
+    t <- toilet
+    if (input$handicap == TRUE){
+      t <- filter(t, Handicap == "Yes")
+    }
+    if (input$yearround == TRUE){
+      t <- filter(t, Yearround == "Yes")
+    }
+    return(t)
+  })
+  
+  # Filter crime data
+  cdata <- reactive({
+    draw <- crime
+    if (input$crime != '') {
+      t <- filter(draw, Offense == input$crime)
+      draw <- t
+    }
+    
+    if (input$month != '') {
+      t <- filter(draw, Occurrence.Month == input$month)
+      draw <- t
+    }
+    
+    if (input$day != '') {
+      t <- filter(draw, Occurrence.Day == input$day)
+      draw <- t
+    }
+    
+    if (input$hour != '') {
+      t <- filter(draw, Occurrence.Hour == input$hour)
+      draw <- t
+    }
+    
+    return(draw)
+  })
+  
+  # Create toiletIcon
+  restroomIcon <- reactive({
+    makeIcon(
+      iconUrl = paste("https://github.com/TZstatsADS/project2-group6/blob/master/doc/toiletIcon/toilet", as.character(input$icon), ".png?raw=true", sep = ""),
+      iconWidth = 25, iconHeight = 25,
+      iconAnchorX = 13, iconAnchorY = 13
+    ) %>%
+      return()
+  }) 
+  
+  a <- as.data.frame(decodeLine("qy}wF`dkbM`EpCdCaIgAs@qHaF{FuDyFuDgC~H^V"))
+  
+  # Add toilet and crime circles to map  
+  observe({  
+    pal1 <- colorFactor(palette()[-1], levels(crime$Offense))
+    Radius1 <- 3
+    if (input$addcrime == TRUE&length(as.matrix(cdata())) != 0){
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        # addPolylines(lng=c(a$lon,a$lon1),lat=c(a$lat,a$lat1),color="red") %>%
+        addMarkers(data = ttype(), ~Long, ~Lat, icon = restroomIcon(), options = markerOptions(opacity = 0.9), popup = ~Name) %>%
+        addCircleMarkers(data = cdata(), ~Long, ~Lat, radius = Radius1, stroke = FALSE, fillOpacity = 0.7, fillColor = pal1(cdata()[["Offense"]])) %>%
+        addLegend("bottomleft", pal=pal1, values=cdata()[["Offense"]], title="crime",
+                  layerId="colorLegend")
+    }
+    else {
+      
+      leafletProxy("map") %>%
+        clearMarkers() %>%
+        # addPolylines(lng=c(a$lon,a$lon1),lat=c(a$lat,a$lat1),color="red") %>%
+        addMarkers(data = ttype(), ~Long, ~Lat, icon = restroomIcon(), options = markerOptions(opacity = 0.9), 
+                   popup = paste("*Name:", ttype()$Name, "<br>",
+                                 "*Address:", ttype()$Address, "<br>")) #%>%
+    }
+  })
+  
+  # Show a circle at the given location
+  show <- reactive({
+    function(eventid, lat, lng) {
+      leafletProxy("map") %>% addCircles(lng=lng,lat=lat, radius=input$circleR, fillColor="red",layerId = eventid, group="overlays")
+    } %>%
+      return()
+  })
+  
+  # When mouseover, show a circle
+  observe({
+    leafletProxy("map") %>% clearGroup("overlays") # %>%
+    # leafletProxy("map") %>%
+    # addPolylines(lng=c(a$lon,a$lon1),lat=c(a$lat,a$lat1),color="red")
+    # addPolylines(lng=a$lon,lat=a$lat,color="red")
+    event <- input$map_marker_mouseover
+    if (is.null(event))
+      return()
+    
+    #         opts(
+    #           panel.background = theme_rect(fill = "transparent",colour = NA), # or theme_blank()
+    #           panel.grid.minor = theme_blank(), 
+    #           panel.grid.major = theme_blank(),
+    #           plot.background = theme_rect(fill = "transparent",colour = NA)
+    #         )
+    # })#,bg="transparent")
+    isolate({
+      show()(event$id, event$lat, event$lng)
+    })
+  })
+  
+  observe({
+    #print(input$submit[1])
+    if(input$submit[1] > 0) {
+      # add <- isolate(input$address)
+      
+      xlatlon<-geoCode(input$address)
+      xlat <- as.numeric(xlatlon[1])
+      xlon <- as.numeric(xlatlon[2])
+      leafletProxy("map") %>% 
+        addMarkers(lng=xlon,lat=xlat)
+      
+      event <- input$map_marker_click
+      if (is.null(event))
+        return()
+      
+      #filtered_crime <- filter_crime(event$lat,event$lng,input$circleR)
+      #output$totalcrime <- renderText({
+      
+      #  if (nrow(filtered_crime) == 0) {
+      #    return(NULL)
+      #  }
+      #  paste("Total Number of Crimes:",nrow(filtered_crime))
+      #})
+      
+      #output$circ_plot <- renderPlot({
+      #  if (nrow(filtered_crime) == 0) {
+      #    return(NULL)
+      #  }
+      
+      #  levels(filtered_crime$Offense)[levels(filtered_crime$Offense) ==  "GRAND LARCENY OF MOTOR VEHICLE"] <- "GTA"
+      #  ggplot(filtered_crime,aes(x=factor(1),fill=as.factor(Offense))) + geom_bar() + coord_polar(theta='y') + ylab("") + xlab("") +
+      #    theme(panel.background= element_blank(),plot.background= element_blank())
+      #})
+      
+      rlat <- event$lat
+      rlon <- event$lng
+      poly <- geoRoute(xlat,xlon,rlat,rlon)
+      llatlon <- decodeLine(poly)
+      # print(llatlon)
+      leafletProxy("map") %>% clearShapes() %>%
+        addPolylines(lng=llatlon$lon,lat=llatlon$lat,color="red") #%>%
+      # addMarkers(lng=xlon,lat=xlat)
+    }
+  })
+  
+  observe({
+    #print(input$submit[1])
+    #if(input$submit[1] > 0) {
+    # add <- isolate(input$address)
+    
+    #xlatlon<-geoCode(input$address)
+    #xlat <- as.numeric(xlatlon[1])
+    #xlon <- as.numeric(xlatlon[2])
+    #leafletProxy("map") %>% 
+    #  addMarkers(lng=xlon,lat=xlat)
+    
+    event <- input$map_marker_click
+    if (is.null(event))
+      return()
+    
+    filtered_crime <- filter_crime(event$lat,event$lng,input$circleR)
+    output$totalcrime <- renderText({
+      
+      if (nrow(filtered_crime) == 0) {
+        return(NULL)
+      }
+      paste("Total Number of Crimes:",nrow(filtered_crime))
+    })
+    
+    output$circ_plot <- renderPlot({
+      if (nrow(filtered_crime) == 0) {
+        return(NULL)
+      }
+      
+      levels(filtered_crime$Offense)[levels(filtered_crime$Offense) ==  "GRAND LARCENY OF MOTOR VEHICLE"] <- "GTA"
+      ggplot(filtered_crime,aes(x=factor(1),fill=as.factor(Offense))) + geom_bar() + coord_polar(theta='y') + ylab("") + xlab("") +
+        theme(panel.background= element_blank(),plot.background= element_blank())
+    })
+    
+    #rlat <- event$lat
+    #rlon <- event$lng
+    #poly <- geoRoute(xlat,xlon,rlat,rlon)
+    #llatlon <- decodeLine(poly)
+    #leafletProxy("map") %>% clearShapes() %>%
+    #  addPolylines(lng=llatlon$lon,lat=llatlon$lat,color="red") #%>%
+    
+    #}
+  })
+  
+  # breaks <- hist(plot=FALSE, )
+  
+  
+  
+  ## Dynamic Map ###########################################
+  
+  # Create the map
+  output$map2 <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "https://api.mapbox.com/v4/mapbox.dark/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhcG9sZW9uIiwiYSI6ImNpa3Q0cXB5bTAwMXh2Zm0zczY1YTNkd2IifQ.rjnjTyXhXymaeYG6r2pclQ",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -73.97, lat = 40.75, zoom = 13)
+  })
+  
+  
+  # Filter crime data
+  drawvalue <- reactive({
+    if (input$offense == ''){
+      t <- filter(crime, minute == input$minute)
+      return(t)
+    }
+    else{
+      t <- filter(crime, Offense == input$offense, minute==input$minute)
+      return(t)
+    }})
+  
+  observe({
+    draw <- drawvalue()
+    pal <- colorFactor(palette()[-1], levels(crime$Offense))
+    radius <-  50
+    if (length(as.matrix(draw)) != 0) {
+      leafletProxy("map2", data = draw) %>%
+        clearShapes() %>%
+        addCircles(~Long, ~Lat, radius=radius,
+                   stroke=FALSE, fillOpacity=0.8,fillColor=pal(draw[["Offense"]])) %>%
+        addLegend("bottomleft", pal=pal, values=levels(draw[["Offense"]]), layerId="colorLegend")
+    }
+    else {
+      leafletProxy("map2", data = draw) %>%
+        clearShapes()
+    }
+    
+    
+  })
+  
 })
